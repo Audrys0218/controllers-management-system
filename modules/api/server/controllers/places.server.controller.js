dcae0e586a2c3cb90926c6c523e5b7cc63e070f5
Audrius Danielevicius <audrius.danielevicius@gmail.com>
2015-10-01 22:28:15 +0300

    Basi api

--- modules/api/server/controllers/places.server.controller.js
+++ modules/api/server/controllers/places.server.controller.js
@@ -1,15 +1,32 @@
 'use strict';
 
-exports.places = function(req, res){
-    res.send([
-        {
-            id: 1,
-            name: "Room-1"
-        },
-        {
-            id: 1,
-            name: "Room-1"
-        }
+var path = require('path'),
+    mongoose = require('mongoose'),
+    Place = mongoose.model('Place'),
+    errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
 
-    ]);
+exports.create = function (req, res) {
+    var place = new Place(req.body);
+    console.log(req.body);
+    place.save(function (err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.json(place);
+        }
+    });
+};
+
+exports.list = function (req, res) {
+    Place.find().sort('-created').exec(function (err, places) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.json(places);
+        }
+    });
 };
--- modules/api/server/routes/api.server.routes.js
+++ modules/api/server/routes/api.server.routes.js
@@ -4,5 +4,8 @@ module.exports = function (app) {
   // Root routing
   var places = require('../controllers/places.server.controller');
 
-  app.route('/places').get(places.places);
+  app.route('/places').post(places.create);
+
+  app.route('/places').get(places.list);
+
 };
