d7f87fdf8193e6f4a7feaf17461d22562f97f205
Zygimantas Butkus <zygjys@gmail.com>
2015-10-01 22:29:56 +0300

    added routes

--- modules/api/server/controllers/sensors.server.controller.js
+++ modules/api/server/controllers/sensors.server.controller.js
@@ -1,15 +1,26 @@
 'use strict';
 
-exports.places = function(req, res){
+exports.list = function(req, res){
     res.send([
         {
-            id: 1,
-            name: "Room-1"
-        },
-        {
-            id: 1,
-            name: "Room-1"
+            status: "list"
         }
-
     ]);
 };
+
+exports.create = function(req, res){
+    res.send([
+        {
+            status: "created"
+        }
+    ]);
+};
+
+exports.delete = function(req, res){
+    res.send([
+        {
+            status: "deleted"
+        }
+    ]);
+};
+
--- modules/api/server/routes/api.server.routes.js
+++ modules/api/server/routes/api.server.routes.js
@@ -2,10 +2,16 @@
 
 module.exports = function (app) {
   // Root routing
-  var places = require('../controllers/places.server.controller');
+  var places = require('../controllers/places.server.controller'),
+      sensors = require('../controllers/sensors.server.controller');
 
   app.route('/places').post(places.create);
 
   app.route('/places').get(places.list);
 
+  app.route('/places').get(places.places);
+
+  app.route('/sensors').get(sensors.list);
+  app.route('/sensors').post(sensors.create);
+  app.route('/sensors').delete(sensors.delete);
 };
