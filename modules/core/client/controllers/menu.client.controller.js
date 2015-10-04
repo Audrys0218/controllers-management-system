030540505cc8d40f4e6a4efa1ba6aef3a0e99402
Audrius Danielevicius <audrius.danielevicius@gmail.com>
2015-10-04 16:05:16 +0300

    Added bones for menu controller

---
+++ modules/core/client/controllers/menu.client.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+anuglar.module('core').controller(['$scope', function($scope){
+    var menuItems = [
+        {
+            title: 'Places',
+            url: '/places'
+        },
+        {
+            title: 'Sensors',
+            url: '/sensors'
+        }
+    ]
+
+}]);
