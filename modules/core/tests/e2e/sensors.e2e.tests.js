34920aaad1d768e5f2fdd2ab65988c6a9480a801
Zygimantas Butkus <zygjys@gmail.com>
2015-12-04 22:08:27 +0200

    Update sensor tests

--- modules/core/tests/e2e/sensors.e2e.tests.js
+++ modules/core/tests/e2e/sensors.e2e.tests.js
@@ -12,7 +12,7 @@ describe('Sensors E2E Tests:', function () {
         });
 
         it('add sensor', function () {
-            element(by.css('button[ng-click="addEdit()"]')).click();
+            element.all(by.css('button[ng-click="addEdit()"]')).first().click();
 
             element(by.id('title')).sendKeys('test');
             element(by.css('select[ng-model="model.place"] option:nth-child(2)')).click();
@@ -23,8 +23,20 @@ describe('Sensors E2E Tests:', function () {
             expect(element.all(by.repeater('sensor in model.sensors')).count()).toBe(startCount + 1);
         });
 
+        it('update sensor', function () {
+            element.all(by.css('button[ng-click="addEdit(sensor.id)"]')).first().click();
+
+            element(by.id('title')).clear().sendKeys('test2');
+            element(by.css('button[ng-click="save()"]')).click();
+
+            expect(element.all(by.repeater('sensor in model.sensors')).count()).toBe(startCount);
+            element.all(by.css("td.sensor-title")).first().getText().then(function (text) {
+                expect(text).toBe('test2');
+            });
+        });
+
         it('delete sensor', function () {
-            element(by.css('button[ng-click="delete(sensor.id)"]')).click();
+            element.all(by.css('button[ng-click="delete(sensor.id)"]')).first().click();
             element(by.css('button[ng-click="ok()"]')).click();
 
             expect(element.all(by.repeater('sensor in model.sensors')).count()).toBe(startCount - 1);
