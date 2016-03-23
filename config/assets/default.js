018707f62e961ae2efe270951c99b7f57929b331
Audrius Danielevicius <audrius.danielevicius@gmail.com>
2016-03-24 00:43:31 +0200

    Added bootsrap and jquery to project javascript files

--- config/assets/default.js
+++ config/assets/default.js
@@ -16,7 +16,9 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/bootstrap/dist/js/bootstrap.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
