e8b238797feab45fa7719fc18e149e61fed14b39
Audrius Danielevicius <audrius.danielevicius@gmail.com>
2016-04-18 23:29:06 +0300

    Added loader to mirocontroller

--- modules/core/client/services/microcontrollers/microcontrollers.client.model.js
+++ modules/core/client/services/microcontrollers/microcontrollers.client.model.js
@@ -5,38 +5,46 @@ angular.module('core')
         function ($http, confirmation) {
 
             var model = {
-                microcontrollers: []
+                microcontrollers: [],
+                loading: false
             };
 
             var load = function () {
+                model.loading = true;
                 return $http({
                     method: 'GET',
                     url: '/api/v1/microcontroller'
                 }).then(function (response) {
                     model.microcontrollers = response.data;
-                });
+                }).finally(removeLoader)
             };
 
             var deleteController = function (controllerId) {
                 confirmation.confirm('Warning!', 'Do you really want to delete this item?', function () {
+                    model.loading = true;
                     $http({
                         method: 'DELETE',
                         url: '/api/v1/microcontroller/' + controllerId
-                    }).then(load);
+                    }).then(load).finally(removeLoader)
                 });
             };
 
             var save = function (microcontroller) {
+                model.loading = true;
                 if (microcontroller.id) {
-                    return $http.put('/api/v1/microcontroller/' + microcontroller.id, microcontroller).then(load);
+                    return $http.put('/api/v1/microcontroller/' + microcontroller.id, microcontroller).then(load).finally(removeLoader);
                 } else {
-                    return $http.post('/api/v1/microcontroller', microcontroller).then(load);
+                    return $http.post('/api/v1/microcontroller', microcontroller).then(load).finally(removeLoader);
                 }
             };
 
             var get = function (id) {
                 return $http.get('api/v1/microcontroller/' + id);
             };
+
+            function removeLoader() {
+                model.loading = false;
+            }
 
             return {
                 model: model,
